9f09616e05339b0926dad61268b42f4d98aed682
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2023-11-04 11:22:23 +0300

    Update Matrix.cs

--- NM_Lab1/NM_Lab1/Matrix.cs
+++ NM_Lab1/NM_Lab1/Matrix.cs
@@ -1,4 +1,4 @@
-﻿// Систему уравнений задают шесть векторов: a, b, c, f, p, q
+// Систему уравнений задают шесть векторов: a, b, c, f, p, q
 // a, b, c - векторы для элементов матрицы А, расположенных на нижней кодиагонали, на главной диагонали и на верхней диагонали
 // p, q - векторы для элементов k-й строки и k-ого столбца матрицы А (1 < k < n)
 // f - вектор правой части системы уравнений
@@ -58,31 +58,31 @@ class Matrix
             {
                 if (col == numberK - 1)
                 {
-                    vectorQ[row] = left + random.NextDouble()*(right - left);
+                    vectorQ[row] = left + random.NextDouble() * (right - left);
                 }
                 if (row == 0)
                 {
                     vectorA[row] = 0;
-                    vectorB[row] = left + random.NextDouble()*(right-left);
-                    vectorC[row] = left + random.NextDouble()*(right-left);
+                    vectorB[row] = left + random.NextDouble() * (right - left);
+                    vectorC[row] = left + random.NextDouble() * (right - left);
                 }
                 if ((row >= 1 && row <= numberK - 1 - 3) || (row >= numberK - 1 + 1 && row <= matrixSize - 1 - 1))
                 {
-                    vectorA[row] = left + random.NextDouble()*(right-left);
-                    vectorB[row] = left + random.NextDouble()*(right-left);
-                    vectorC[row] = left + random.NextDouble()*(right-left);
+                    vectorA[row] = left + random.NextDouble() * (right - left);
+                    vectorB[row] = left + random.NextDouble() * (right - left);
+                    vectorC[row] = left + random.NextDouble() * (right - left);
                 }
                 if (row == matrixSize - 1)
                 {
-                    vectorA[row] = left + random.NextDouble()*(right-left);
-                    vectorB[row] = left + random.NextDouble()*(right-left);
+                    vectorA[row] = left + random.NextDouble() * (right - left);
+                    vectorB[row] = left + random.NextDouble() * (right - left);
                     vectorC[row] = 0;
                 }
                 if (row >= numberK - 1 - 2 && row <= numberK - 1 - 1)
                 {
-                    vectorA[row] = left + random.NextDouble()*(right-left);
-                    vectorB[row] = left + random.NextDouble()*(right-left);
-                    vectorC[row] = left + random.NextDouble()*(right-left);
+                    vectorA[row] = left + random.NextDouble() * (right - left);
+                    vectorB[row] = left + random.NextDouble() * (right - left);
+                    vectorC[row] = left + random.NextDouble() * (right - left);
                     if (row == numberK - 1 - 2)
                     {
                         vectorQ[row] = vectorC[row];
@@ -94,7 +94,7 @@ class Matrix
                 }
                 if (row == numberK - 1)
                 {
-                    vectorP[col] = left + random.NextDouble()*(right-left);
+                    vectorP[col] = left + random.NextDouble() * (right - left);
                     if (col == numberK - 1 - 2)
                     {
                         vectorC[numberK - 1] = vectorP[numberK - 1 - 2];
@@ -110,17 +110,73 @@ class Matrix
                     }
                 }
             }
-            vectorF[row] = left + random.NextDouble()*(right-left);
+            vectorF[row] = left + random.NextDouble() * (right - left);
         }
 
         this.left = left;
         this.right = right;
     }
 
-    public void PrintMatrix()
+    //Доделать преобразования для вектора f
+    public void StepFirst()
     {
-        Console.WriteLine("Matrix Size: " + matrixSize);
-        Console.WriteLine("Number K: " + numberK);
+        for (int row = 0; row < numberK - 1 - 1; row++)
+        {
+            double koef = vectorB[row];
+            vectorB[row] /= koef;
+            vectorC[row] /= koef;
+            vectorQ[row] /= koef;
+
+            koef = vectorA[row + 1];
+            vectorA[row + 1] -= koef * vectorB[row];
+            vectorB[row + 1] -= koef * vectorC[row];
+            vectorQ[row + 1] -= koef * vectorQ[row];
+
+            koef = vectorP[matrixSize - 1 - row];
+            vectorP[matrixSize - 1 - row] -= koef * vectorB[row];
+            vectorP[matrixSize - 1 - row - 1] -= koef * vectorC[row];
+        }
+    }
+
+    //Доделать преобразования для вектора f
+    public void StepSecond()
+    {
+        for (int row = matrixSize - 1; row > numberK - 1; row--)
+        {
+            double koef = vectorB[row];
+            vectorB[row] /= koef;
+            vectorA[row] /= koef;
+            vectorQ[row] /= koef;
+
+            koef = vectorC[row - 1];
+            vectorC[row - 1] -= koef * vectorB[row];
+            vectorB[row - 1] -= koef * vectorA[row];
+            vectorQ[row - 1] -= koef * vectorQ[row];
+
+            koef = vectorP[matrixSize - 1 - row];
+            vectorP[matrixSize - 1 - row] -= koef * vectorB[row];
+            vectorP[matrixSize - 1 - row + 1] -= koef * vectorA[row];
+        }
+
+        double koef_dop = vectorB[numberK - 1];
+        vectorB[numberK - 1] /= koef_dop;
+        vectorP[numberK - 1 - 1] = vectorB[numberK - 1];
+
+        koef_dop = vectorC[numberK - 1 - 1];
+        vectorC[numberK - 1 - 1] -= koef_dop * vectorB[numberK - 1];
+    }
+
+    //Шаг 3: начинаем искать решения
+    public void StepThird()
+    {
+
+    }
+
+    //Сделать вывод для вектора f
+    public void PrintMatrixToFile(StreamWriter sw)
+    {
+        sw.WriteLine("Matrix Size: " + matrixSize);
+        sw.WriteLine("Number K: " + numberK);
 
         int end_col = matrixSize - 1;
         for (int row = 0; row < matrixSize; row++)
@@ -129,71 +185,31 @@ class Matrix
             {
                 if (col == numberK - 1)
                 {
-                    Console.Write(String.Format("{0}  ", vectorQ[row]));
+                    sw.Write(String.Format("{0:f12}  ", vectorQ[row]));
                 }
                 else if (row == numberK - 1)
                 {
-                    Console.Write(String.Format("{0}  ",vectorP[col]));
+                    sw.Write(String.Format("{0:f12}  ", vectorP[col]));
                 }
                 else if (col == end_col - 1)
                 {
-                    Console.Write(String.Format("{0}  ",vectorC[row]));
+                    sw.Write(String.Format("{0:f12}  ", vectorC[row]));
                 }
                 else if (col == end_col)
                 {
-                    Console.Write(String.Format("{0}  ",vectorB[row]));
+                    sw.Write(String.Format("{0:f12}  ", vectorB[row]));
                 }
                 else if (col == end_col + 1)
                 {
-                    Console.Write(String.Format("{0}  ",vectorA[row]));
+                    sw.Write(String.Format("{0:f12}  ", vectorA[row]));
                 }
                 else
                 {
-                    Console.Write(String.Format("{0, 15}  ", 0));
+                    sw.Write(String.Format("{0:f12}  ", 0.0));
                 }
             }
             end_col--;
-            Console.WriteLine();
+            sw.WriteLine();
         }
     }
-
-    public void printToFile(StreamWriter sw)
-    {
-		sw.WriteLine("Matrix Size: " + matrixSize);
-		sw.WriteLine("Number K: " + numberK);
-
-		int end_col = matrixSize - 1;
-		for (int row = 0; row < matrixSize; row++)
-		{
-			for (int col = 0; col < matrixSize; col++)
-			{
-				if (col == numberK - 1)
-				{
-					sw.Write(String.Format("{0}  ", vectorQ[row]));
-				}
-				else if (row == numberK - 1)
-				{
-					sw.Write(String.Format("{0}  ", vectorP[col]));
-				}
-				else if (col == end_col - 1)
-				{
-					sw.Write(String.Format("{0}  ", vectorC[row]));
-				}
-				else if (col == end_col)
-				{
-					sw.Write(String.Format("{0}  ", vectorB[row]));
-				}
-				else if (col == end_col + 1)
-				{
-					sw.Write(String.Format("{0}  ", vectorA[row]));
-				}
-				else
-				{
-					sw.Write(String.Format("{0, 15}  ", 0));
-				}
-			}
-			end_col--;
-			sw.WriteLine();
-		}
-	}
 }
