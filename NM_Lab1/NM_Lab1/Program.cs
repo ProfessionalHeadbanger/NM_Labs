3c457da04c4f1e3efefcf6e0131e031956b51679
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2023-11-04 11:22:53 +0300

    Update Program.cs

--- NM_Lab1/NM_Lab1/Program.cs
+++ NM_Lab1/NM_Lab1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http.Headers;
 using System.IO;
 
@@ -8,11 +8,21 @@ class Program
     {
         Console.WriteLine("Размер матрицы: ");
         int n = Convert.ToInt32(Console.ReadLine());
-        int k = n/2 + 1;
+        int k = n / 2 + 1;
 
         Matrix matrix = new Matrix(n, k, -10, 10);
-        StreamWriter sw = new StreamWriter("C:\\Users\\Всеволод\\Desktop\\C#Labs\\ConsoleApp1\\ConsoleApp1\\Test.txt");
-        matrix.printToFile(sw);
-        sw.Close();
+        StreamWriter initialMatrix = new StreamWriter("D:\\Лабы\\ЧМ\\NM_Lab1\\NM_Lab1\\initialMatrix.txt");
+        matrix.PrintMatrixToFile(initialMatrix);
+        initialMatrix.Close();
+
+        matrix.StepFirst();
+        StreamWriter step1 = new StreamWriter("D:\\Лабы\\ЧМ\\NM_Lab1\\NM_Lab1\\step1.txt");
+        matrix.PrintMatrixToFile(step1);
+        step1.Close();
+
+        matrix.StepSecond();
+        StreamWriter step2 = new StreamWriter("D:\\Лабы\\ЧМ\\NM_Lab1\\NM_Lab1\\step2.txt");
+        matrix.PrintMatrixToFile(step2);
+        step2.Close();
     }
 }
