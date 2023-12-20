5088a3c6571fafc5fff86e05f6e32b87b1a9e721
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2023-12-20 18:25:58 +0300

    Add files via upload

---
+++ NM_Lab3/NM_Lab3/Matrix.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+class RandomGenerator
+{
+    private decimal left;
+    private decimal right;
+    Random random = new Random();
+
+    public RandomGenerator(decimal left, decimal right)
+    {
+        this.left = left;
+        this.right = right;
+    }
+
+    public decimal GetRandomDecimalValue()
+    {
+        var item = new decimal(random.NextDouble());
+        return left + item * (right - left);
+    }
+}
+
+class Matrix
+{
+    public decimal[,] matrix;
+    public int size;
+    public decimal[,] generated_lambda;
+    public decimal[] diagonalElements;
+
+    public Matrix(int size)
+    {
+        matrix = new decimal[size, size];
+        this.size = size;
+        generated_lambda = new decimal[size, size];
+        diagonalElements = new decimal[size];
+    }
+
+    public void Generate(decimal left, decimal right)
+    {
+        RandomGenerator rnd = new RandomGenerator(left, right);
+        decimal[] omega = new decimal[size];
+        decimal summ = 0;
+        for (int i = 0; i < size; i++)
+        {
+            omega[i] = rnd.GetRandomDecimalValue();
+            summ += omega[i]*omega[i];
+        }
+        for (int i = 0; i < size; i++)
+        {
+            omega[i] /= summ;
+        }
+        decimal[,] wwt = new decimal[size, size];
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                wwt[row, col] = 2 * omega[row] * omega[col];
+            }
+        }
+        decimal[,] E = new decimal[size, size];
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                if (row == col)
+                {
+                    E[row, col] = 1;
+                }
+            }
+        }
+        decimal[,] H = new decimal[size, size];
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                H[row, col] = E[row, col] - wwt[row, col];
+            }
+        }
+        
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                if (row == col)
+                {
+                    generated_lambda[row, col] = rnd.GetRandomDecimalValue();
+                }
+            }
+        }
+        decimal[,] TransposedH = Transpose(H);
+        decimal[,] H_Lambda = Multiply(H, generated_lambda);
+        matrix = Multiply(H_Lambda, TransposedH);
+    }
+
+    public decimal[,] Transpose(decimal[,] matrix)
+    {
+        decimal[,] TransposedMatrix = new decimal[size, size];
+
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                TransposedMatrix[col, row] = matrix[row, col];
+            }
+        }
+
+        return TransposedMatrix;
+    }
+
+    public decimal[,] Multiply(decimal[,] first, decimal[,] second)
+    {
+        decimal[,] MultiplyedMatrix = new decimal[size, size];
+
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                decimal sum = 0;
+                for (int k = 0; k < size; k++)
+                {
+                    sum += first[row, k] * second[k, col];
+                }
+                MultiplyedMatrix[col, row] = sum;
+            }
+        }
+
+        return MultiplyedMatrix;
+    }
+
+    public void PrintToFile(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    writer.Write(string.Format("{0:f16} ", matrix[row, col]));
+                }
+                writer.WriteLine();
+            }
+            writer.WriteLine();
+        }
+    }
+
+    public void PrintLambdaToFile(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            writer.WriteLine("Obtained eigenvalues:");
+            for (int row = 0; row < size; row++)
+            {
+                writer.Write("λ" + (row + 1) + $" = {diagonalElements[row]:f16}");
+                writer.WriteLine();
+            }
+            writer.WriteLine();
+        }
+    }
+
+    public void PrintGeneratedLambdaToFile(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            writer.WriteLine("Generated eigenvalues:");
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (row == col)
+                    {
+                        writer.Write("λ" + (row + 1) + $" = {diagonalElements[row]:f16}");
+                        writer.WriteLine();
+                    }
+                }
+            }
+            writer.WriteLine();
+        }
+    }
+}
+
---
+++ NM_Lab3/NM_Lab3/Program.cs
@@ -0,0 +1,93 @@
+﻿class Program
+{
+    public static void RightJacobiMethod(Matrix matrix, decimal tolerance, int maxIterations)
+    {
+        decimal[,] rotationMatrix = new decimal[matrix.size, matrix.size];
+
+        for (int i = 0; i < matrix.size; i++)
+        {
+            matrix.diagonalElements[i] = matrix.matrix[i, i];
+            rotationMatrix[i, i] = 1.0M;
+        }
+
+        for (int iteration = 0; iteration < maxIterations; iteration++)
+        {
+            int p, q;
+            FindMaxOffDiagonalElement(matrix, out p, out q);
+
+            decimal maxOffDiagonal = Math.Abs(matrix.matrix[p, q]);
+            if (maxOffDiagonal < tolerance)
+            {
+                break;
+            }
+
+            decimal theta = 0.5M * (decimal)Math.Atan2((double)(2 * matrix.matrix[p, q]), (double)(matrix.diagonalElements[q] - matrix.diagonalElements[p]));
+
+            rotationMatrix[p, p] = rotationMatrix[q, q] = (decimal)Math.Cos((double)theta);
+            rotationMatrix[p, q] = (decimal)Math.Sin((double)theta);
+            rotationMatrix[q, p] = -(decimal)Math.Sin((double)theta);
+
+            matrix.matrix = matrix.Multiply(matrix.matrix, rotationMatrix);
+            matrix.diagonalElements[p] = matrix.matrix[p, p];
+            matrix.diagonalElements[q] = matrix.matrix[q, q];
+        }
+    }
+
+    static void FindMaxOffDiagonalElement(Matrix matrix, out int p, out int q)
+    {
+        decimal maxElement = 0.0M;
+        p = q = 0;
+
+        for (int i = 0; i < matrix.size - 1; i++)
+        {
+            for (int j = i + 1; j < matrix.size; j++)
+            {
+                decimal currentElement = Math.Abs(matrix.matrix[i, j]);
+                if (currentElement > maxElement)
+                {
+                    maxElement = currentElement;
+                    p = i;
+                    q = j;
+                }
+            }
+        }
+    }
+
+
+
+    public static void Main()
+    {
+        bool menu = true;
+        while (menu)
+        {
+            Console.WriteLine("1) Найти собственные значения матрицы\n2) Набор тестов\n3) Выход");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Размер матрицы: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    Matrix generated_matrix = new Matrix(n);
+                    generated_matrix.Generate(-2, 2);
+                    generated_matrix.PrintToFile("D:\\Лабы\\ЧМ\\NM_Lab3\\NM_Lab3\\initial_generated_matrix.txt");
+                    Console.Write("Максимальный по модулю внедиагональный элемент: ");
+                    decimal tolerance = Convert.ToDecimal(Console.ReadLine());
+                    Console.Write("Максимальное число поворотов: ");
+                    int maxIterations = Convert.ToInt32(Console.ReadLine());
+                    RightJacobiMethod(generated_matrix, tolerance, maxIterations);
+                    generated_matrix.PrintLambdaToFile("D:\\Лабы\\ЧМ\\NM_Lab3\\NM_Lab3\\generated_matrix_values.txt");
+                    generated_matrix.PrintGeneratedLambdaToFile("D:\\Лабы\\ЧМ\\NM_Lab3\\NM_Lab3\\generated_matrix_values.txt");
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                    menu = false;
+                    break;
+                default:
+                    Console.WriteLine("Неправильный ввод");
+                    break;
+            }
+        }
+    }
+}
