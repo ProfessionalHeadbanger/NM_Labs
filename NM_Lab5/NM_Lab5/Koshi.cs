5401ba0616d64cfd3817d3d7e002aed12c295220
Vsevolod <45267974+ProfessionalHeadbanger@users.noreply.github.com>
2024-05-14 13:35:17 +0300

    Add files via upload

---
+++ NM_Lab5/NM_Lab5/Koshi.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Koshi
+{
+    public double A;
+    public double B;
+    public double C;
+    public double yC;
+    public double hMin;
+    public double eps;
+    public double y;
+    public double y1, y2;
+    public double k1, k2, k3, k4, k5, k6;
+    public double h;
+
+    public int dotsCount = 0;
+    public int badDotsCount = 0;
+    public int minDotsCount = 0;
+
+    public Koshi() { }
+
+    public void InputFromFile(string path)
+    {
+        using (StreamReader reader = new StreamReader("E:\\Лабы\\ЧМ\\NM_Lab5\\NM_Lab5\\" + path))
+        {
+            string[] line1 = reader.ReadLine().Split(' ');
+            A = double.Parse(line1[0]);
+            B = double.Parse(line1[1]);
+            C = double.Parse(line1[2]);
+            yC = double.Parse(line1[3]);
+            string[] line2 = reader.ReadLine().Split(' ');
+            hMin = double.Parse(line2[0]);
+            eps = double.Parse(line2[1]);
+            y = yC;
+            h = Math.Abs(B - A) / 10;
+        }
+    }
+
+    public static double func_calc(double x, double y)
+    {
+        //return 3 * x + 2 * y - 4 * x * x;
+        //return 2 * x + y - x * x;
+        return 2 * x + y - x*x;
+        //return 2 * x * x;
+    }
+
+    public void calculateElementsPositive(double x)
+    {
+        k1 = h * func_calc(x, y);
+        k2 = h * func_calc(x + h / 2, y + k1 / 2);
+        k3 = h * func_calc(x + h / 2, y + k1 / 4 + k2 / 4);
+        k4 = h * func_calc(x + h, y - k2 + 2 * k3);
+        k5 = h * func_calc(x + 2 * h / 3, y + 7 * k1 / 27 + 10 * k2 / 27 + k4 / 27);
+        k6 = h * func_calc(x + h * h / 5, y - (28 * k1 - 125 * k2 + 546 * k3 + 54 * k4 + 378 * k5) / 625);
+        y1 = y + (k1 + 4 * k3 + k4) / 6;
+        y2 = y + (14 * k1 + 35 * k4 + 162 * k5 + 125 * k6)/336;
+    }
+
+    public void calculateElementsNegative(double x)
+    {
+        k1 = h * func_calc(x, y);
+        k2 = h * func_calc(x - h / 2, y - k1 / 2);
+        k3 = h * func_calc(x - h / 2, y - k1 / 4 - k2 / 4);
+        k4 = h * func_calc(x - h, y + k2 - 2 * k3);
+        k5 = h * func_calc(x - 2 * h / 3, y - 7 * k1 / 27 - 10 * k2 / 27 - k4 / 27);
+        k6 = h * func_calc(x - h * h / 5, y + (28 * k1 - 125 * k2 + 546 * k3 + 54 * k4 + 378 * k5) / 625);
+        y1 = y - (k1 + 4 * k3 + k4) / 6;
+        y2 = y - (14 * k1 + 35 * k4 + 162 * k5 + 125 * k6)/336;
+    }
+
+    public bool Condition()
+    {
+        return (-42 * k1 - 224 * k3 - 21 * k4 + 162 * k5 + 125 * k6) / 336 > eps;
+    }
+
+    public void PrintStep(double x)
+    {
+        Console.WriteLine("X: " + x + "; Y: " + y + "; E: " + ((-42 * k1 - 224 * k3 - 21 * k4 + 162 * k5 + 125 * k6) / 336) + "; H: " + h);
+    }
+
+    public void PrintPointsStats()
+    {
+        Console.WriteLine("Число точек интегрирования: " + dotsCount + "; Точность не достигается: " + badDotsCount + "; Минимальные шаги: " + minDotsCount);
+    }
+
+    public void PrintInitial()
+    {
+        Console.WriteLine("A: " + A + "; B: " + B + "; C: " + C + "; yC: " + yC);
+    }
+}
+
---
+++ NM_Lab5/NM_Lab5/Program.cs
@@ -0,0 +1,257 @@
+﻿
+
+class Program
+{
+    public static int Calculate(Koshi koshi)
+    {
+        if (koshi.A == koshi.C)
+        {
+            double x = koshi.A;
+            while (koshi.B - (x + koshi.h) >= koshi.hMin)
+            {
+                koshi.calculateElementsPositive(x);
+
+                if (koshi.Condition())
+                {
+                    if (koshi.h == koshi.hMin)
+                    {
+                        koshi.y = koshi.y1;
+                        x += koshi.h;
+                        koshi.PrintStep(x);
+                        koshi.badDotsCount++;
+                        koshi.minDotsCount++;
+                    }
+                    else
+                    {
+                        koshi.h = Math.Min(koshi.h / 2, koshi.hMin);
+                    }
+                }
+                else
+                {
+                    koshi.y = koshi.y1;
+                    x += koshi.h;
+                    koshi.PrintStep(x);
+                    koshi.dotsCount++;
+                    if (koshi.h == koshi.hMin)
+                    {
+                        koshi.minDotsCount++;
+                    }
+                    koshi.h *= 2;
+                }
+            }
+
+            if (koshi.B - x >= 2 * koshi.hMin)
+            {
+                koshi.h = (koshi.B - koshi.hMin) - x;
+                koshi.calculateElementsPositive(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.minDotsCount += 2;
+                koshi.dotsCount += 2;
+                koshi.y = koshi.y1;
+                x += koshi.h;
+                koshi.PrintStep(x);
+                koshi.h = koshi.hMin;
+                koshi.calculateElementsPositive(x);
+                koshi.y = koshi.y1;
+                x += koshi.h;
+                koshi.PrintStep(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.PrintPointsStats();
+                if (koshi.Condition())
+                {
+                    return 1;
+                }
+            }
+            else if (koshi.B - x <= 1.5*koshi.hMin)
+            {
+                koshi.h = koshi.B - x;
+                koshi.calculateElementsPositive(x);
+                koshi.minDotsCount++;
+                koshi.dotsCount++;
+                koshi.y = koshi.y1;
+                x += koshi.h;
+                koshi.h = koshi.hMin;
+                koshi.PrintStep(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.PrintPointsStats();
+                if (koshi.Condition())
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                koshi.h = (koshi.B - x) / 2;
+                koshi.calculateElementsPositive(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.minDotsCount += 2;
+                koshi.dotsCount += 2;
+                koshi.y = koshi.y1;
+                x += koshi.h;
+                koshi.PrintStep(x);
+                koshi.calculateElementsPositive(x);
+                koshi.y = koshi.y1;
+                x += koshi.h;
+                koshi.PrintStep(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.PrintPointsStats();
+                if (koshi.Condition())
+                {
+                    return 1;
+                }
+            }
+        }
+        else
+        {
+            double x = koshi.B;
+            while((x - koshi.h) - koshi.A >= koshi.hMin)
+            {
+                koshi.calculateElementsNegative(x);
+                if (koshi.Condition())
+                {
+                    if (koshi.h == koshi.hMin)
+                    {
+                        koshi.y = koshi.y1;
+                        x -= koshi.h;
+                        koshi.PrintStep(x);
+                        koshi.badDotsCount++;
+                        koshi.minDotsCount++;
+                    }
+                    else
+                    {
+                        koshi.h = Math.Min(koshi.h / 2, koshi.hMin);
+                    }
+                }
+                else
+                {
+                    koshi.y = koshi.y1;
+                    x -= koshi.h;
+                    koshi.PrintStep(x);
+                    koshi.dotsCount++;
+                    if (koshi.h == koshi.hMin)
+                    {
+                        koshi.minDotsCount++;
+                    }
+                    koshi.h *= 2;
+                }
+            }
+            if (x - koshi.A >= 2*koshi.hMin)
+            {
+                koshi.h = x - (koshi.A + koshi.hMin);
+                koshi.calculateElementsNegative(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.minDotsCount += 2;
+                koshi.dotsCount += 2;
+                koshi.y = koshi.y1;
+                x -= koshi.h;
+                koshi.h = koshi.hMin;
+                koshi.PrintStep(x);
+                koshi.calculateElementsNegative(x);
+                koshi.y = koshi.y1;
+                x -= koshi.h;
+                koshi.PrintStep(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.PrintPointsStats();
+                if (koshi.Condition())
+                {
+                    return 1;
+                }
+            }
+            else if (x - koshi.A <= 1.5*koshi.hMin)
+            {
+                koshi.h = x - koshi.A;
+                koshi.calculateElementsNegative(x);
+                koshi.minDotsCount++;
+                koshi.dotsCount++;
+                koshi.y = koshi.y1;
+                x -= koshi.h;
+                koshi.h = koshi.hMin;
+                koshi.PrintStep(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.PrintPointsStats();
+                if (koshi.Condition())
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                koshi.h = (x - koshi.A) / 2;
+                koshi.calculateElementsNegative(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.minDotsCount += 2;
+                koshi.dotsCount += 2;
+                koshi.y = koshi.y1;
+                x -= koshi.h;
+                koshi.PrintStep(x);
+                koshi.calculateElementsNegative(x);
+                koshi.y = koshi.y1;
+                x -= koshi.h;
+                koshi.PrintStep(x);
+                if (koshi.Condition())
+                {
+                    koshi.badDotsCount++;
+                }
+                koshi.PrintPointsStats();
+                if (koshi.Condition())
+                {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    public static void Main()
+    {
+        bool menu = true;
+        while (menu)
+        {
+            Console.WriteLine("1) Ввести данные с файла; \n2) Выход");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Введите название файла: ");
+                    string path = Console.ReadLine();
+                    Koshi koshi = new();
+                    koshi.InputFromFile(path);
+                    koshi.PrintInitial();
+                    Console.WriteLine("IER: " + Calculate(koshi));
+                    break;
+                case "2":
+                    menu = false;
+                    break;
+                default:
+                    Console.WriteLine("Неправильный ввод");
+                    break;
+            }
+        }
+    }
+}
